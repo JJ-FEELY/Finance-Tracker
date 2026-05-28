9f881db2985ac8aac2605c70d931e8fd9cb62b81
Justin Feely <60102533+mchaeltfeely@users.noreply.github.com>
2026-05-28 12:04:50 +0100

    ViewModel set up

--- FinanceTracker/View/ContentView.swift
+++ FinanceTracker/View/ContentView.swift
@@ -11,47 +11,33 @@ import SwiftData
 struct ContentView: View {
     
     @Environment(\.modelContext) private var modelContext
+    @StateObject private var viewModel = ContentViewModel()
     
 
     var body: some View {
         NavigationSplitView {
             List {
-                ForEach(items) { item in
+                ForEach(viewModel.items) { item in
                     NavigationLink {
                         Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
                     } label: {
                         Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
                     }
                 }
-                .onDelete(perform: deleteItems)
+                .onDelete(perform: viewModel.deleteItems)
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
                 }
                 ToolbarItem {
-                    Button(action: addItem) {
+                    Button(action: viewModel.addItem) {
                         Label("Add Item", systemImage: "plus")
                     }
                 }
             }
         } detail: {
             Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
         }
     }
 }
--- FinanceTracker/ViewModel/ContentViewModel.swift
+++ FinanceTracker/ViewModel/ContentViewModel.swift
@@ -12,8 +12,38 @@ import _SwiftData_SwiftUI
 
 class ContentViewModel: ObservableObject{
     
-    private let controller = PersistenceController.shared.context
+    private let context = PersistenceController.shared.context
     
     
+    @Published var items: [Item] = []
     
+    init(){
+        fetchItems()
+    }
+    
+    func fetchItems(){
+        let descriptor = FetchDescriptor<Item>()
+        do{
+            items = try context.fetch(descriptor)
+        } catch{
+            print("Fetch failed: \(error)")
+        }
+    }
+    
+    
+    func addItem() {
+        let newItem = Item(timestamp: Date())
+        context.insert(newItem)
+        
+        fetchItems()
+
+    }
+    
+    
+    func deleteItems(offsets: IndexSet) {
+        for index in offsets {
+            context.delete(items[index])
+        }
+        fetchItems()
+    }
 }
